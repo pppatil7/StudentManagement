61ca00ede26b56581e6ba05964805badb146bc11
pp_ patil7 <pundalikpatil7218@gmail.com>
2025-09-07 10:04:01 +0530

    custom auth config

--- src/main/java/com/practice/security/WebConfig.java
+++ src/main/java/com/practice/security/WebConfig.java
@@ -6,18 +6,48 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig {
 
+    private final PasswordEncoder passwordEncoder;
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-
+        httpSecurity
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/enrollments/**").hasRole("ADMIN")
+                        .requestMatchers("/students/**").hasRole("STUDENT")
+                        .requestMatchers("/courses/**").permitAll()
+                )
+                .formLogin(Customizer.withDefaults());
 
         return httpSecurity.build();
+    }
+
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        UserDetails user1 = User.withUsername("admin")
+                .password(passwordEncoder.encode("pass"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user2 = User.withUsername("student")
+                .password(passwordEncoder.encode("1234"))
+                .roles("STUDENT")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
+
     }
 
 
